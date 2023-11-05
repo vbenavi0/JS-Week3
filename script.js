e5f345ecc327a31e4ed34c01bdd04317535037b8
vbenavi0 <victorvill.0988@gmail.com>
2023-11-05 14:55:58 -0500

    completed last 2 challenges

--- script.js
+++ script.js
@@ -39,3 +39,53 @@ console.log(patient2.getName());
 console.log(patient2.getSSN());
 
 //Very Hard: 
+
+class Person {
+    constructor(name, job, age) {
+        this.pName = name;
+        this.job = job;
+        this.age = age;
+    }
+
+    exercise(){
+        console.log("Running is fun!");
+    }
+
+    fetchJob(){
+        console.log(this.pName + " is a " + this.job+".");
+    }
+}
+
+class Programmer extends Person {
+
+    constructor(name, job, age, langs) {
+        super(name, job, age);
+        this.pLangs = langs;
+    }
+
+    learnLanguage(lang){
+        this.pLangs.push(lang);
+    }
+
+    listLanguage(){
+        return this.pLangs;
+    }
+  }
+
+
+const person1 = new Person("Harold", "Backend Engineer", 20);
+const c1 = new Programmer("Liana", "DevOps", 35, ["HTML", "C#", "LUA"]);
+const c2 = new Programmer("Edwin", "janitor", 55, ["HTML", "SASS”, ”Ruby"]);
+const c3 = new Programmer("Manny", "SysOps", 31,[("HTML", "CSS", "JS", "R")]);
+c1.learnLanguage("CSS");
+c2.learnLanguage("C++");
+c3.learnLanguage("JAVA");
+console.log(c1.listLanguage());
+console.log(c2.listLanguage());
+console.log(c3.listLanguage());
+console.log(person1);
+console.log(c1);
+console.log(c2);
+console.log(c3);
+person1.exercise();
+person1.fetchJob();
